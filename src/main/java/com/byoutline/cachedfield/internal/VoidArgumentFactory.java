f534938b829e8c02bd49068930a86afefb4a70aa
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-03-11 19:59:02 +0100

    Added: VoidArgumentFactory file that was forgotten in commit 6aa7fc90c1c94f907b3186a72b3520e0d44cdfa2

---
+++ src/main/java/com/byoutline/cachedfield/internal/VoidArgumentFactory.java
@@ -0,0 +1,54 @@
+package com.byoutline.cachedfield.internal;
+
+import com.byoutline.cachedfield.ErrorListener;
+import com.byoutline.cachedfield.ErrorListenerWithArg;
+import com.byoutline.cachedfield.ProviderWithArg;
+import com.byoutline.cachedfield.SuccessListener;
+import com.byoutline.cachedfield.SuccessListenerWithArg;
+
+import javax.annotation.Nonnull;
+import javax.inject.Provider;
+
+/**
+ * Utility that converts clesses without args to classes with Void arg.
+ *
+ * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
+ */
+public final class VoidArgumentFactory {
+
+    private VoidArgumentFactory() {
+    }
+
+    public static <RETURN_TYPE> ProviderWithArg<RETURN_TYPE, Void> addVoidArg(
+            @Nonnull Provider<RETURN_TYPE> valueGetter) {
+        return new ProviderWithArg<RETURN_TYPE, Void>() {
+
+            @Override
+            public RETURN_TYPE get(Void arg) {
+                return valueGetter.get();
+            }
+        };
+    }
+
+    public static <RETURN_TYPE> SuccessListenerWithArg<RETURN_TYPE, Void> addVoidArg(
+            @Nonnull SuccessListener<RETURN_TYPE> successHandler) {
+        return new SuccessListenerWithArg<RETURN_TYPE, Void>() {
+
+            @Override
+            public void valueLoaded(RETURN_TYPE value, Void arg) {
+                successHandler.valueLoaded(value);
+            }
+        };
+    }
+
+    public static <RETURN_TYPE> ErrorListenerWithArg<Void> addVoidArg(
+            @Nonnull ErrorListener errorHandler) {
+        return new ErrorListenerWithArg<Void>() {
+
+            @Override
+            public void valueLoadingFailed(Exception ex, Void arg) {
+                errorHandler.valueLoadingFailed(ex);
+            }
+        };
+    }
+}
