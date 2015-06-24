a26d2c58875e771aa43dc041c23b0c2d268eda08
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-06-24 15:21:34 +0200

    Added: Executors constructor to CachedFieldImpl.

--- src/main/java/com/byoutline/cachedfield/CachedFieldImpl.java
+++ src/main/java/com/byoutline/cachedfield/CachedFieldImpl.java
@@ -4,7 +4,11 @@ import com.byoutline.cachedfield.internal.StubErrorListener;
 import com.byoutline.cachedfield.internal.VoidArgumentFactory;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.inject.Provider;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Default implementation of {@link CachedField}. Loads value on separate thread
@@ -48,11 +52,20 @@ public class CachedFieldImpl<RETURN_TYPE> implements CachedField<RETURN_TYPE> {
             @Nonnull Provider<RETURN_TYPE> valueGetter,
             @Nonnull SuccessListener<RETURN_TYPE> successHandler,
             @Nonnull ErrorListener errorHandler) {
+        this(sessionProvider, valueGetter, successHandler, errorHandler, Executors.newCachedThreadPool(), null);
+    }
+
+    public CachedFieldImpl(@Nonnull Provider<String> sessionProvider,
+                           @Nonnull Provider<RETURN_TYPE> valueGetter,
+                           @Nonnull SuccessListener<RETURN_TYPE> successHandler,
+                           @Nonnull ErrorListener errorHandler,
+                           @Nonnull ExecutorService valueGetterExecutor,
+                           @Nullable Executor stateListenerExecutor) {
         ProviderWithArg<RETURN_TYPE, Void> valueGetterWithArg = VoidArgumentFactory.addVoidArg(valueGetter);
         SuccessListenerWithArg<RETURN_TYPE, Void> success = VoidArgumentFactory.addVoidArg(successHandler);
         ErrorListenerWithArg<Void> error = VoidArgumentFactory.addVoidArg(errorHandler);
         delegate = new CachedFieldWithArgImpl<RETURN_TYPE, Void>(sessionProvider,
-                valueGetterWithArg, success, error);
+                valueGetterWithArg, success, error, valueGetterExecutor, stateListenerExecutor);
     }
 
     @Override
