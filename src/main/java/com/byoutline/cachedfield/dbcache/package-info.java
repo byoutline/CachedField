8eed226f6cd6e7296aaa84af3322ce3c2e3a9540
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-08-03 16:25:29 +0200

    Javadoc: Add package-info files.

---
+++ src/main/java/com/byoutline/cachedfield/dbcache/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Utility classes for {@link com.byoutline.cachedfield.CachedField} that allows easier
+ * use when cached data is supposed to be download from API and backed up in local db.
+ */
+package com.byoutline.cachedfield.dbcache;
